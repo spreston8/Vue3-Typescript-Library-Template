a4ae78c70d3cd72c5b7684fb8aae29138fee3936
Stephen Preston <60404443+spreston8@users.noreply.github.com>
2022-01-13 15:09:20 -0800

    remove references

--- src/components/HeaderUtilBar.vue
+++ src/components/HeaderUtilBar.vue
@@ -1,12 +1,10 @@
 <script>
-import HeaderUtilBarLink from "./HeaderUtilBarLink.vue";
 // import { defineComponent } from 'vue';
 /**
  * Provides utilities (links) above HeaderBar
  */
 export default {
   name: "HeaderUtilBar",
-  components: { HeaderUtilBarLink },
   props: {
     /**
      * Text Color
@@ -20,42 +18,12 @@ export default {
 </script>
 <template>
   <div class="header-util-bar">
-    <img
-      class="util-bar-icon"
-      src="https://www.cnb.com/content/dam/cnb/global-assets/images/icons/topnav_contactus.svg"
-    />
-    <header-util-bar-link
-      :textColor="textColor"
-      href="https://www.cnb.com/about-us/company-overview.html"
-      class="header-util-bar_icons-aboutus"
-      title="About Us"
-    >
-      About Us
-    </header-util-bar-link>
-    <img
-      class="util-bar-icon"
-      src="https://www.cnb.com/content/dam/cnb/global-assets/images/icons/topnav_locations.svg"
-    />
-    <header-util-bar-link
-      :textColor="textColor"
-      href="https://locations.cnb.com/"
-      class="header-util-bar_icons-locations"
-      title="Locations"
-    >
-      Locations
-    </header-util-bar-link>
-    <img
-      class="util-bar-icon"
-      src="https://www.cnb.com/content/dam/cnb/global-assets/images/icons/topnav_aboutus.svg"
-    />
-    <header-util-bar-link
-      :textColor="textColor"
-      href="https://www.cnb.com/about-us/contact-us.html"
-      class="header-util-bar_icons-contactus"
-      title="Contact Us"
-    >
-      Contact Us
-    </header-util-bar-link>
+    <img class="util-bar-icon" src="" />
+    About Us
+    <img class="util-bar-icon" src="" />
+    Locations
+    <img class="util-bar-icon" src="" />
+    Contact Us
   </div>
 </template>
 <style>
